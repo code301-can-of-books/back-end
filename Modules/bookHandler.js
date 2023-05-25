1aeac8923be1e06ca6392474e97f64c9d5bab65f
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-05-24 20:05:07 -0700

    :construction: Post & Delete

---
+++ Modules/bookHandler.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Book = require('../Model/book');
+
+const bookHandler = {};
+
+bookHandler.getBook = function (req, res) {
+  let queryObject = {};
+
+  if (req.query._id) {
+    queryObject = { id: req.query._id };
+  }
+  console.log(queryObject);
+  Book.find(queryObject)
+    .then((data) => res.status(200).send(data))
+    .catch((err) => console.error(err));
+};
+
+bookHandler.postBook = function (req, res, next) {
+  const data = req.body;
+
+  Book.create(data)
+    .then((createBook) => res.status(201).send(createBook))
+    .catch((err) => next(err));
+};
+
+bookHandler.deleteBook = function (req, res, next) {
+  const { _id } = req.params;
+  console.log(_id);
+  Book.findByIdAndDelete(_id)
+    .then((deletedBook) => res.status(200).send('Book has been deleted'))
+    .catch((err) => next(err));
+};
+module.exports = bookHandler;
--- clear.js
+++ clear.js
@@ -6,14 +6,14 @@ mongoose.connect(process.env.MONGODB_URL);
 const Book = require('./Model/book');
 
 async function clear() {
-    try{
-      await Book.deleteMany({});
-      console.log('Books cleared');
-    } catch (err) {
-      console.error(err)
-    } finally {
-      mongoose.disconnect();
-    }
+  try {
+    await Book.deleteMany({});
+    console.log('Books cleared');
+  } catch (err) {
+    console.error(err);
+  } finally {
+    mongoose.disconnect();
+  }
 }
 
 clear();
